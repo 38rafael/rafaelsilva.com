f9b0d76e0e7bbba3d9d1a6bb43dbd1caf3979fee
Rafael Silva <rafaelribeirodasilva7@gmail.com>
2020-07-05 13:46:58 -0300

    mudei o nome de uma arquivo

--- Verifica_login.php
+++ Verifica_login.php
@@ -5,7 +5,7 @@ if (isset($_POST['email']) && !empty($_POST['email'])) {
 	
 	$email = $_POST['email'];
 	$senha = md5($_POST['senha']);
-
+	
 	$sql = "SELECT * FROM usuarios";
 	$query = mysqli_query($con, $sql);
 	$fetch = mysqli_fetch_assoc($query);
@@ -16,7 +16,7 @@ if (isset($_POST['email']) && !empty($_POST['email'])) {
 		header('Location: restrito.php');
 	}else {
 		header('Location: login.php');
-	}
+ 	}
 }
 
 ?>
